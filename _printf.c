064b6fd55901aac04488cc632e07f40df4411751
MomoHesham <mernah2012@gmail.com>
2023-05-30 16:09:55 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -4,17 +4,15 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format
- * Return: printed chars
+ * @format: format.
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
-	int i, print = 0, print_char = 0;
+	int i, print = 0, print_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-
-	char buffer(BUFF_SIZE);
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
@@ -28,7 +26,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			print_chars++
+			print_chars++;
 		}
 		else
 		{
@@ -45,6 +43,7 @@ int _printf(const char *format, ...)
 			print_chars += print;
 		}
 	}
+
 	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
@@ -53,9 +52,9 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - prints the contents of the buffer if it exist
- * @buffer: array of chars
- * @buff_ind: index at which to add next char, represents the length
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
@@ -64,3 +63,4 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
+
